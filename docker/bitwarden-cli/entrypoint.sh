7b56374c861d017d915f59414e745c6751b3e1ae
Lucas Declercq <lucas.declercq@agicap.com>
2023-07-27 13:43:32 +0200

    feat(bitwarden-cli): use api key login method

--- docker/bitwarden-cli/entrypoint.sh
+++ docker/bitwarden-cli/entrypoint.sh
@@ -2,11 +2,8 @@
 
 set -e
 
-bw config server "${BW_HOST}"
-
-export BW_SESSION="$(bw login ${BW_USER} --passwordenv BW_PASSWORD --raw)"
-
-bw unlock --check
+bw login --apikey
+export BW_SESSION="$(bw unlock --passwordenv BW_PASSWORD --raw)"
 
 echo "Running bitwarden webhook server on port 8087"
 bw serve --hostname 0.0.0.0 #--disable-origin-protection
