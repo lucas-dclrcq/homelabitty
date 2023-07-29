de37bf884f63591c92c972e633c7da4f3f51d47a
Lucas Declercq <lucas.declercq@agicap.com>
2023-07-29 14:33:16 +0200

    fix(bitwarden-cli): use npm to install bw to have a arm64 compatible version

--- docker/bitwarden-cli/entrypoint.sh
+++ docker/bitwarden-cli/entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-bw login --apikey
-export BW_SESSION="$(bw unlock --passwordenv BW_PASSWORD --raw)"
+./node_modules/@bitwarden/cli/build/bw.js login --apikey
+export BW_SESSION="$(./node_modules/@bitwarden/cli/build/bw.js unlock --passwordenv BW_PASSWORD --raw)"
 
 echo "Running bitwarden webhook server on port 8087"
-bw serve --hostname 0.0.0.0 #--disable-origin-protection
+./node_modules/@bitwarden/cli/build/bw.js serve --hostname 0.0.0.0 #--disable-origin-protection
