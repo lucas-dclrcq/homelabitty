5afa148b8401e6030fa58ef91d557e0080da777f
Lucas Declercq <lucas.declercq@agicap.com>
2024-08-26 13:58:45 +0200

    chore: remove legacy directory

--- legacy/docker/custom/bitwarden-cli/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-./node_modules/@bitwarden/cli/build/bw.js login --apikey
-export BW_SESSION="$(./node_modules/@bitwarden/cli/build/bw.js unlock --passwordenv BW_PASSWORD --raw)"
-
-echo "Running bitwarden webhook server on port 8087"
-./node_modules/@bitwarden/cli/build/bw.js serve --hostname 0.0.0.0 #--disable-origin-protection
