1de50652014ad05d2f8c0d43fd65df2d25c1e7b1
Lucas Declercq <lucas.declercq@agicap.com>
2024-08-29 20:44:55 +0200

    feat(vault): add unseal script

---
+++ scripts/unseal.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+VAULT_UNSEAL_KEY=$(jq -r ".unseal_keys_b64[]" $1)
+
+kubens vault
+kubectl exec -ti vault-0 -- vault operator unseal "$VAULT_UNSEAL_KEY"
+kubectl exec -ti vault-1 -- vault operator unseal "$VAULT_UNSEAL_KEY"
+kubectl exec -ti vault-2 -- vault operator unseal "$VAULT_UNSEAL_KEY"
+
+kubens -
