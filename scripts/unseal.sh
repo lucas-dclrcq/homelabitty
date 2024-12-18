40992e678890d638bbf0ef15a72c6f65ea33e919
Lucas Declercq <lucas.declercq@agicap.com>
2024-12-18 19:05:17 +0100

    feat(scripts): do not go back to previous ns in unseal script

--- scripts/unseal.sh
+++ scripts/unseal.sh
@@ -6,5 +6,3 @@ kubens vault
 kubectl exec -ti vault-0 -- vault operator unseal "$VAULT_UNSEAL_KEY"
 kubectl exec -ti vault-1 -- vault operator unseal "$VAULT_UNSEAL_KEY"
 kubectl exec -ti vault-2 -- vault operator unseal "$VAULT_UNSEAL_KEY"
-
-kubens -
