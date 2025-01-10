a452422066c3856bdde45eff181e0eb2cb8ea685
Lucas Declercq <lucas.declercq@agicap.com>
2025-01-10 22:29:00 +0100

    fix(slskd): try to fix betanin script

--- kubernetes/apps/media/slskd/app/resources/notify_betanin.sh
+++ kubernetes/apps/media/slskd/app/resources/notify_betanin.sh
@@ -1,24 +1,9 @@
-#!/usr/bin/env bash
-# shellcheck disable=SC2154
+#!/bin/bash
 
-set -euo pipefail
+name=$(echo "$1" | awk -F'"localDirectoryName": "' '{print $2}' | awk -F'",' '{print $1}')
 
-if [ -n "$BETANIN_API_KEY" ] || [ -n "$BETANIN_URL" ]; then
-    echo "BETANIN_API_KEY and BETANIN_URL variables are mandatory"
-    exit 1;
-fi
-
-DIRECTORY_PATH=$(echo "$1" | grep -oP '"localDirectoryName":\s*"\K[^"]+')
-
-status_code=$(curl \
-    --silent \
-    --write-out "%{http_code}" \
-    --output /dev/null \
-    --request POST \
-    --header "Content-Type: application/x-www-form-urlencoded" \
-    --header "X-API-Key: ${BETANIN_API_KEY}" \
-    --data-urlencode "path=${DIRECTORY_PATH}" \
-    "$BETANIN_URL/api/torrents"
-)
-
-echo -e "Betanin notification returned with HTTP status code ${status_code}\n"
+wget -q -O/dev/null \
+     --post-data "name=$name&path=${name}" \
+     --header="X-API-KEY: $BETANIN_API_KEY" \
+     --header="User-Agent: slskd_to_betanin.sh" \
+      "$BETANIN_URL/api/torrents"
