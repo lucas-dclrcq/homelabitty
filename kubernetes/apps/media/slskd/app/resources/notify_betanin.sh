21dadb3e64cc5b73afbda74ab5a2f02fdd08c454
Lucas Declercq <lucas.declercq@agicap.com>
2025-01-17 22:47:13 +0100

    fix(slskd): try to fix betanin script

--- kubernetes/apps/media/slskd/app/resources/notify_betanin.sh
+++ kubernetes/apps/media/slskd/app/resources/notify_betanin.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+echo "$1"
+
 name=$(echo "$1" | awk -F'"localDirectoryName": "' '{print $2}' | awk -F'",' '{print $1}')
 
-wget -q -O/dev/null \
-     --post-data "name=$name&path=${name}" \
-     --header="X-API-KEY: $BETANIN_API_KEY" \
-     --header="User-Agent: slskd_to_betanin.sh" \
-      "$BETANIN_URL/api/torrents"
+echo "$name"
+
+wget --post-data "name=$name&path=$name" --header="X-API-KEY: $BETANIN_API_KEY" "$BETANIN_URL/api/torrents"
