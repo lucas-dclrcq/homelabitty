65c0087901870760eeab97ba3920d44e615f8d86
Lucas Declercq <lucas.declercq@agicap.com>
2025-01-18 22:57:42 +0100

    feat(slskd): add debug logs to betanin script

--- kubernetes/apps/media/slskd/app/resources/notify_betanin.sh
+++ kubernetes/apps/media/slskd/app/resources/notify_betanin.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "$1"
+>&2 echo "Payload: $1"
 
 name=$(echo "$1" | awk -F'"localDirectoryName": "' '{print $2}' | awk -F'",' '{print $1}')
 
-echo "$name"
+>&2 echo "Name: $name"
 
 wget --post-data "name=$name&path=$name" --header="X-API-KEY: $BETANIN_API_KEY" "$BETANIN_URL/api/torrents"
