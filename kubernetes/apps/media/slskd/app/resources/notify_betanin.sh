238a327d5f3f27f7d2ad8a346addad4c5d726c2a
Lucas Declercq <lucas.declercq@agicap.com>
2025-01-19 15:01:12 +0100

    feat(slskd): more debuuug

--- kubernetes/apps/media/slskd/app/resources/notify_betanin.sh
+++ kubernetes/apps/media/slskd/app/resources/notify_betanin.sh
@@ -2,7 +2,4 @@
 
 name=$(echo "$1" | awk -F'"localDirectoryName": "' '{print $2}' | awk -F'",' '{print $1}')
 
-echo "Payload: $1" >> /config/debug.txt
-echo "Name: $name" >> /config/debug.txt
-
 wget --post-data "name=$name&path=$name" --header="X-API-KEY: $BETANIN_API_KEY" "$BETANIN_URL/api/torrents"
