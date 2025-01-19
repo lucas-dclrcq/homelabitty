187ab03e9ef3ee4f794bac87a51dfb5deff45e8b
Lucas Declercq <lucas.declercq@agicap.com>
2025-01-19 14:22:14 +0100

    feat(slskd): log to file

--- kubernetes/apps/media/slskd/app/resources/notify_betanin.sh
+++ kubernetes/apps/media/slskd/app/resources/notify_betanin.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
->&2 echo "Payload: $1"
-
 name=$(echo "$1" | awk -F'"localDirectoryName": "' '{print $2}' | awk -F'",' '{print $1}')
 
->&2 echo "Name: $name"
+echo "Payload: $1" >> /config/debug.txt
+echo "Name: $name" >> /config/debug.txt
 
 wget --post-data "name=$name&path=$name" --header="X-API-KEY: $BETANIN_API_KEY" "$BETANIN_URL/api/torrents"
