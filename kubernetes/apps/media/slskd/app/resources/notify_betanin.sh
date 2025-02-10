26566788a650ca04c6338c86e5f47803fb01ba95
Lucas Declercq <lucas.declercq@agicap.com>
2025-02-10 10:29:00 +0100

    fix(slskd): use @SevereOverfl0w script

--- kubernetes/apps/media/slskd/app/resources/notify_betanin.sh
+++ kubernetes/apps/media/slskd/app/resources/notify_betanin.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-name=$(echo "$1" | awk -F'"localDirectoryName": "' '{print $2}' | awk -F'",' '{print $1}')
+read -t5 -r line
+path=$(echo "$line" | awk -F'"localDirectoryName":"' '{print $2}' | awk -F'"' '{print $1}' | awk '{printf "%s", $0}')
 
-wget --post-data "name=$name&path=$name" --header="X-API-KEY: $BETANIN_API_KEY" "$BETANIN_URL/api/torrents"
+wget --quiet \
+    --header="X-API-Key: $BETANIN_API_KEY" \
+    --header="User-Agent: slskd-to-betanin" \
+    --post-data="both=${path}" \
+    "$BETANIN_URL/api/torrents" -O -
