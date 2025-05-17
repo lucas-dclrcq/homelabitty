10271a70b9353ca3d888853d26ec908512993e91
Lucas Declercq <lucas.declercq@agicap.com>
2025-05-17 21:45:50 +0200

    feat: add script to scale nas apps down or up

---
+++ scripts/scale_nas_apps.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+if [ $# -eq 0 ]; then
+  echo "Usage: $0 [up|down]"
+  echo "  up   - Scale nas apps to 1 replicas"
+  echo "  down - Scale nas apps to replicas"
+  exit 1
+fi
+
+if [ "$1" = "up" ]; then
+  replicas=1
+elif [ "$1" = "down" ]; then
+  replicas=0
+else
+  echo "Invalid parameter: $1"
+  echo "Usage: $0 [up|down]"
+  exit 1
+fi
+
+echo "Scaling NAS applications with replicas=$replicas"
+
+kubectl scale deployment -n default filebrowser  --replicas $replicas
+kubectl scale deployment -n default immich-microservices  --replicas $replicas
+kubectl scale deployment -n default immich-machine-learning  --replicas $replicas
+kubectl scale deployment -n default immich-server  --replicas $replicas
+kubectl scale deployment -n downloads bazarr  --replicas $replicas
+kubectl scale deployment -n downloads lidarr  --replicas $replicas
+kubectl scale deployment -n downloads qbittorrent  --replicas $replicas
+kubectl scale deployment -n downloads radarr  --replicas $replicas
+kubectl scale deployment -n downloads readarr  --replicas $replicas
+kubectl scale deployment -n downloads sabnzbd  --replicas $replicas
+kubectl scale deployment -n downloads sonarr  --replicas $replicas
+kubectl scale deployment -n home frigate  --replicas $replicas
+kubectl scale deployment -n infrastructure minio --replicas $replicas
+kubectl scale deployment -n media audiobookshelf --replicas $replicas
+kubectl scale deployment -n media jellyfin --replicas $replicas
+kubectl scale deployment -n media komga --replicas $replicas
+kubectl scale deployment -n media navidrome --replicas $replicas
+
+echo "NAS applications scaled $1"
